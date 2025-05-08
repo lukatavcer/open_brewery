5d5ccfdd3695c22a1faf842775c927ceb284a46a
Luka Tavčer <lukatavcer@yahoo.com>
2025-05-08 20:21:40 +0200

    Add Filtering & eslint & prettier

---
+++ src/types/brewery.ts
@@ -0,0 +1,9 @@
+// Define the brewery interface
+export interface Brewery {
+  id: string;
+  name: string;
+  brewery_type: string;
+  city: string;
+  state: string;
+  website_url: string | null;
+}
